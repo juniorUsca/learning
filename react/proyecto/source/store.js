1a5cca8e56dbaa384af4235361206385f1a4e1bc
juniorUsca <junior.usca@gmail.com>
2017-09-08 13:02:59 -0500

    React-blog: Uso de middlewares

--- react/proyecto/source/store.js
+++ react/proyecto/source/store.js
@@ -1,8 +1,29 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import { createLogger } from 'redux-logger'
 
 import reducer from './reducer'
 
-// 2DO parametro: recibe el estao inicial o un enhancer
-const store = createStore(reducer);
+// mi middleware logger
+// puede usarse para recoger errores y guardar en sentry
+// o para hacer analytics
+//
+// const logger = store => next => (action) => {
+//   console.group("logger")
+//   console.debug('estado actual', store.getState())
+//   console.debug('accion', action)
+//   const result = next(action) // pasa la cccion al siguiente middleware
+//   console.debug('estado nuevo', store.getState())
+//   console.groupEnd("logger")
+//   return result;
+// }
+
+// 2DO parametro: recibe el estado inicial o un enhancer(el enhancer puede ser un middleware)
+const store = createStore(
+  reducer,
+  applyMiddleware(
+    // logger,
+    createLogger(),
+  ),
+);
 
 export default store;
