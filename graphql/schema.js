bd0a37168feda95e098b8527a7911acd7069623e
juniorUsca <junior.usca@gmail.com>
2017-12-09 18:37:22 -0500

    Graphql: Desafio. Terminar los resolvers

--- graphql/schema.js
+++ graphql/schema.js
@@ -56,8 +56,33 @@ const resolvers = {
     Curso: {
         profesor: () => {
             return {
-                nombre: 'pablo'
+                nombre: 'pablo',
+                nacionalidad: 'Peru'
             }
+        },
+        comentarios: () => {
+            return [{
+                id: 1,
+                nombre: 'comentario1',
+                cuerpo: 'el curso esta bueno',
+            }, {
+                id: 2,
+                nombre:'comentario2',
+                cuerpo: 'el curso no me gusta',
+            }]
+        }
+    },
+    Profesor: {
+        cursos: () => {
+            return [{
+                id: 1,
+                titulo: 'curso de graphql',
+                descripcion: 'aprendiendo graphql'
+            }, {
+                id: 2,
+                titulo: 'curso de php',
+                descripcion: 'aprendiendo php'
+            }]
         }
     }
 }
