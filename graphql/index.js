0ddb3f2e522be2a6dd9a14cfc4832891e15c48e0
juniorUsca <junior.usca@gmail.com>
2017-12-09 11:02:31 -0500

    Graphql: Inicializacion del Proyecto

---
+++ graphql/index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const { graphqlExpress, graphiqlExpress } = require('apollo-server-express')
+
+const schema = require('./schema')
+
+const app = express()
+app.use(
+    '/graphql',
+    bodyParser.json(),
+    graphqlExpress({ schema })
+)
+
+app.use(
+    '/graphiql',
+    graphiqlExpress({
+        endpointURL: '/graphql' // donde esta el servidor de grapqhl
+    })
+)
+
+const PORT = 5678
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo ${PORT}`)
+})
