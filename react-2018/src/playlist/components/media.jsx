3d8568ae481e217fcb26ae547bd416c0b5c8714a
junior.usca <junior.usca@gmail.com>
2018-04-22 19:58:15 -0500

    React-2018: Enlazando eventos del DOM

--- react-2018/src/playlist/components/media.jsx
+++ react-2018/src/playlist/components/media.jsx
@@ -4,6 +4,17 @@ import PropTypes from 'prop-types'
 import './media.css'
 
 class Media extends Component {
+
+  constructor(props) {
+    super(props)
+    // this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick = (event) => {
+    console.log(event)
+    console.log(this.props.title)
+  }
+
   render() {
 
     const styles = {
@@ -14,7 +25,7 @@ class Media extends Component {
     }
 
     return(
-      <div className='Media'>
+      <div className='Media' onClick={this.handleClick}>
         <div className='Media-cover'>
           <img
             src = {this.props.image}
