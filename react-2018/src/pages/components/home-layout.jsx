ce7ee5d15eb347fa9f36c2c8c1280d08b0c54791
junior.usca <junior.usca@gmail.com>
2018-04-30 06:33:58 -0500

    React-2018: Estructura del proyecto con Containers y UI components: Contenido relacionado y estilos

--- react-2018/src/pages/components/home-layout.jsx
+++ react-2018/src/pages/components/home-layout.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 
+import './home-layout.css'
+
 function HomeLayout(props) {
   return(
-    <section>
+    <section className='HomeLayout'>
       {props.children}
     </section>
   )
--- react-2018/src/pages/containers/home.jsx
+++ react-2018/src/pages/containers/home.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 
 import HomeLayout from '../components/home-layout.jsx'
 import Categories from '../../categories/components/categories.jsx'
+import Related from '../../related/components/related.jsx'
 
 class Home extends Component {
   constructor(props) {
@@ -12,6 +13,7 @@ class Home extends Component {
   render() {
     return(
       <HomeLayout>
+        <Related />
         <Categories categories={this.props.data.categories} />
       </HomeLayout>
     )
