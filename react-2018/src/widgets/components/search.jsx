5966c79067824d1dd7739269e85464b51f1f6a33
junior.usca <junior.usca@gmail.com>
2018-05-05 12:50:22 -0500

    React-2018: Referencias a eventos de html y formularios

--- react-2018/src/widgets/components/search.jsx
+++ react-2018/src/widgets/components/search.jsx
@@ -3,11 +3,18 @@ import React from 'react'
 import './search.css'
 
 const Search = (props) => (
-  <form className='Search'>
+  <form
+    className='Search'
+    onSubmit={props.handleSubmit}
+  >
     <input
+      ref={props.setRef} /* se ejecuta cuando el elemento es renderizado*/
       type='text'
       placeholder='Busca tu video favorito'
       className='Search-input'
+      name='search'
+      defaultValue='Luis Fonsi'
+      onChange={props.handleChange}
     />
   </form>
 )
--- react-2018/src/widgets/containers/search-container.jsx
+++ react-2018/src/widgets/containers/search-container.jsx
@@ -3,9 +3,32 @@ import React, { Component } from 'react';
 import Search from '../components/search.jsx'
 
 class SearchContainer extends Component {
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.input.value, 'submit')
+  }
+
+  setInputRef = (element) => {
+    this.input = element
+    console.log(this.input, 'referenciandooo')
+  }
+  handleInputChange = (event) => {
+    /*this.setState({
+      value: event.target.value,
+    })*/
+    console.log(event.target.value, 'cambiado de')
+    this.input.value = event.target.value.replace(/ /g,'-')
+    console.log(this.input.value, 'cambiado a')
+  }
+
   render() {
     return (
-      <Search />
+      <Search
+        setRef={this.setInputRef}
+        handleSubmit={this.handleSubmit}
+        handleChange={this.handleInputChange}
+      />
     )
   }
 }
