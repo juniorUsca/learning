76a2052e7e18d9f6bf66f835bdce0feb5644b445
junior.usca <junior.usca@gmail.com>
2018-05-11 23:45:29 -0500

    React-2018: Componentes de titulo y fuente de video

---
+++ react-2018/src/player/components/video.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+import './Video.css'
+
+class Video extends Component {
+  render() {
+    return (
+      <div className="Video">
+        <video
+          autoPlay={this.props.autoplay}
+          src={this.props.src}
+        />
+      </div>
+    )
+  }
+}
+
+export default Video;
--- react-2018/src/player/containers/video-player.jsx
+++ react-2018/src/player/containers/video-player.jsx
@@ -1,14 +1,18 @@
 import React, { Component } from 'react'
 import VideoPlayerLayout from '../components/video-player-layout.jsx';
+import Video from '../components/video.jsx'
+import Title from '../components/title.jsx'
 
 class VideoPlayer extends Component {
   render() {
     return (
       <VideoPlayerLayout>
-        <video
-          src="http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4"
-          autoPlay
-          controls
+        <Title
+          title='Esto es un video!!'
+        />
+        <Video
+          autoplay={true}
+          src='http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4'
         />
       </VideoPlayerLayout>
     )
