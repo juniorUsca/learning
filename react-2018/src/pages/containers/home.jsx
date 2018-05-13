a00ad2f3f50a434a9f14f8ec74507f930f0e5a17
junior.usca <junior.usca@gmail.com>
2018-05-13 13:32:39 -0500

    React-2018: barra de progreso

--- react-2018/src/pages/containers/home.jsx
+++ react-2018/src/pages/containers/home.jsx
@@ -35,7 +35,7 @@ class Home extends Component {
         <HomeLayout>
           <Related />
           <VideoPlayer
-            autoplay={false}
+            autoplay
           />
           <Categories
             categories={this.props.data.categories}
--- react-2018/src/player/containers/video-player.jsx
+++ react-2018/src/player/containers/video-player.jsx
@@ -5,6 +5,7 @@ import Title from '../components/title.jsx'
 import PlayPause from '../components/play-pause.jsx'
 import Timer from '../components/timer.jsx'
 import VideoPlayerControls from '../components/video-player-controls.jsx'
+import ProgressBar from '../components/progress-bar.jsx'
 
 import Utils from './../../utils/utils.jsx'
 
@@ -12,8 +13,8 @@ class VideoPlayer extends Component {
 
   state = {
     pause: false,
-    duration: Utils.formattedTime(0),
-    currentTime: Utils.formattedTime(0),
+    duration: 0,
+    currentTime: 0,
   }
 
   togglePlay = (event) => {
@@ -31,14 +32,22 @@ class VideoPlayer extends Component {
   handleLoadedMetadata = (event) => {
     this.video = event.target
     this.setState({
-      duration: Utils.formattedTime(this.video.duration),
+      duration: this.video.duration,
     })
   }
 
   handleTimeUpdate = (event) => {
     this.setState({
-      currentTime: Utils.formattedTime(this.video.currentTime),
+      currentTime: this.video.currentTime,
     })
+    console.log('changing')
+  }
+
+  handleProgressChange = (event) => {
+    console.log('set')
+    this.video.pause()
+    this.video.currentTime = event.target.value
+    this.video.play()
   }
 
   render() {
@@ -53,8 +62,13 @@ class VideoPlayer extends Component {
             handleClick={this.togglePlay}
           />
           <Timer
+            duration={Utils.formattedTime(this.state.duration)}
+            currentTime={Utils.formattedTime(this.state.currentTime)}
+          />
+          <ProgressBar
             duration={this.state.duration}
-            currentTime={this.state.currentTime}
+            value={this.state.currentTime}
+            handleChange={this.handleProgressChange}
           />
         </VideoPlayerControls>
         <Video
